1111235f48d39694f78db2a51aed6ebc825ffd8e
Sai240924 <saivamshi.0024@gmail.com>
2025-10-24 19:51:01 +0530

    Restore iac-project to repository for Vercel deployment

---
+++ iac-project/main.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    random = {
+      source = "hashicorp/random"
+    }
+  }
+}
+
+provider "random" {}
+
+resource "random_pet" "server" {}
+output "pet_name" {
+  value = random_pet.server.id
+  description = "My pet name is"
+}
